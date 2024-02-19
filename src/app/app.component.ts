e04f8bd9aea40cd9578e7ecc3c0ca79ed0349895
Rakesh60 <rakeshhupta5353@gmail.com>
2024-02-19 16:53:03 +0530

    data fetched

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {PostService} from './services/post.service'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'noteapp';
+  posts:any;
+  constructor(private service:PostService){
+
+  }
+  ngOnInit(): void {
+    this.service.getPosts().subscribe(res=>{
+      this.posts=res;
+      console.log(this.posts)
+    })
+  }
+  
 }
